301f157a6ffe85a7cd6d33bae09101790f9a5e64
Sreenu Kuruva <sreenu.k@marketsimplified.com>
2026-02-21 13:22:16 +0530

    Multi-Step Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+type FormData={
+  name:string
+  email:string
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+function App(){
+  const [step,setStep]=useState<number>(1)
+  const [formData,setFormData]=useState<FormData>({
+    name:"",
+    email:""
+  })
+  const[errors,setErrors]=useState<{name?: string;email?:string}>({})
+  const validateStepOne=()=>{
+    const newErrors:{name?:string}={}
+
+    if(!formData.name.trim()){
+      newErrors.name="Name is required"
+    }
+    setErrors(newErrors)
+    return
+    Object.keys(newErrors).length===0
+  }
+  const validateStepTwo=()=>{
+      const newErrors:{email?:string}={}
+      if(!formData.email.trim()){
+      newErrors.email="Email is required"
+    } 
+    else if(!/^\S+@\S+\.\S+$/.test(formData.email)){
+      newErrors.email="Invalid email format"
+    }  
+    setErrors(newErrors)
+    return
+     Object.keys(newErrors).length===0
+  }
+  const handleNext=()=>{
+    if(step===1 && validateStepOne()){
+      setStep(2)
+      setErrors({})
+    }
+  }
+  const handleBack=()=>{
+    setStep(1)
+    setErrors({})
+  }
+  const handleSubmit=()=>{
+    if(validateStepTwo()){
+      alert("Form submitted successfully")
+      console.log(formData)
+    }
+  }
+  return(
+    <div className="container">
+      <h2>Two Step Form</h2>
+      {step===1 &&(
+        <div className="form-step">
+          <label>Name:</label>
+          <input type="text"
+          value={formData.name}
+          onChange={(e)=>
+            setFormData({...formData,name:e.target.value})
+        }
+        />
+        {errors.name && <p className="error">{errors.name}</p>}
+      <button type="button" onClick ={handleNext}>Next</button>
+    </div>
+    )}
+    {step===2 &&(
+      <div className="form-step">
+        <label>Email</label>
+        <input type="text"
+        value={formData.email}
+        onChange={(e)=>setFormData({...formData,email:e.target.value})}
+        />
+        {errors.email && <p className="error">{errors.email}</p>}
+     <div className="button-group">
+      <button onClick={handleBack}>Back</button>
+       <button onClick={handleSubmit}>Submit</button>
+     </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    )}
+    </div>
   )
 }
 
